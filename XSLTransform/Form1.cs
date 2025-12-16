b6759151573f8f6ccf1161124f14c0865f95524c
DevilRabe <klass10a.nik@gmail.com>
2025-12-16 21:39:53 +0300

    2) После xslt-преобразования, дописывает в элемент Employee атрибут, который отражает сумму всех amount/@salary этого элемента

--- XSLTransform/Form1.cs
+++ XSLTransform/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Xml.Linq;
 using System.Xml.Xsl;
 
 namespace XSLTransform
@@ -11,9 +13,53 @@ namespace XSLTransform
 
         private void BtnRun_Click(object sender, EventArgs e)
         {
-            var xslt = new XslCompiledTransform();
-            xslt.Load("transform.xsl");
-            xslt.Transform("Data1.xml", "Employees.xml");
+            try
+            {
+                var xslt = new XslCompiledTransform();
+                xslt.Load("transform.xsl");
+                xslt.Transform("Data1.xml", "Employees.xml");
+
+                AddTotalAttributeToEmployees();
+
+                MessageBox.Show("Преобразование и обновление выполнены успешно!", "Готово",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void AddTotalAttributeToEmployees()
+        {
+            if (!File.Exists("Employees.xml"))
+                throw new FileNotFoundException("Файл Employees.xml не найден после XSLT-преобразования.");
+
+            var doc = XDocument.Load("Employees.xml");
+            var culture = CultureInfo.InvariantCulture;
+
+            foreach (var employee in doc.Root.Elements("Employee"))
+            {
+                double total = 0.0;
+
+                foreach (var salary in employee.Elements("salary"))
+                {
+                    var amountAttr = salary.Attribute("amount");
+                    if (amountAttr?.Value != null)
+                    {
+                        string amountStr = amountAttr.Value.Replace(",", ".");
+                        if (double.TryParse(amountStr, NumberStyles.Float, culture, out double value))
+                        {
+                            total += value;
+                        }
+                    }
+                }
+                employee.SetAttributeValue("total", total.ToString(culture));
+            }
+
+            doc.Save("Employees.xml");
         }
     }
 }
